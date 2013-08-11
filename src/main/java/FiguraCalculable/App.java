1f001f74a097e6812bdf469b4543e2a333da4170
JIMG <murciaracnido@gmail.com>
2013-08-10 20:03:16 -0500

    figura calculable

---
+++ src/main/java/FiguraCalculable/App.java
@@ -0,0 +1,13 @@
+package FiguraCalculable;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
